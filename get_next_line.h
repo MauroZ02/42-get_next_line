ad6e0cf4bcb3413a5db210402f7d449738624d28
Mauro Z. <maurozangaro2002@gmail.com>
2024-11-18 19:35:23 +0100

    testing the uploaded

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzangaro <mzangaro@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 13:30:14 by mzangaro          #+#    #+#             */
+/*   Updated: 2024/11/13 17:13:25 by mzangaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdio.h>
+// #  ifndef MAX_FD
+// #   define MAX_FD 1024
+// #  endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t n, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+#endif
